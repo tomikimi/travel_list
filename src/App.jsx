5068a4cf5c8837b7d7e3d62dcefbbf27089e9e7d
Tomikimi <tomikimiogoli@yahoo.com>
2026-02-18 14:17:47 +0100

    Addition of styles to Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,69 @@
-function Logo() {}
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
 
-function Form() {}
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
 
-function PackingList() {}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+      <select>
+        <option value={1}>1</option>
+        <option value={2}>2</option>
+        <option value={3}>3</option>
+      </select>
+      <input type="text" placeholder="item..." />
+      <button>Add</button>
+    </div>
+  );
+}
 
-function Stats() {}
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
 
-function App() {}
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity}
+        {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
 
-export default App();
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
+
+function App() {
+  return (
+    <div className="app">
+      <Logo></Logo>
+      <Form></Form>
+      <PackingList></PackingList>
+      <Stats></Stats>
+    </div>
+  );
+}
+
+export default App;
