2d5ece877d5a652ea6740737d9d3530f57299be3
Tomikimi <tomikimiogoli@yahoo.com>
2026-02-19 13:20:43 +0100

    Implementation of Sort Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -52,10 +52,26 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItems }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             onDeleteItem={onDeleteItem}
@@ -64,6 +80,14 @@ function PackingList({ items, onDeleteItem, onToggleItems }) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
@@ -101,7 +125,7 @@ function Stats({ items }) {
       <em>
         {percentage === 100
           ? `You got everything! Ready to go 🚀`
-          : `🧳You have ${numItems} items on your list, and you already packed{" "}
+          : `🧳You have ${numItems} items on your list, and you already packed
         ${numPacked} (${percentage}%)`}
       </em>
     </footer>
